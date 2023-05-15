8a46045a208b767982a00d060960537d7b217641
luqasshh <l_mat@wp.pl>
2023-05-15 14:31:05 +0200

    add base styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
 import React from "react";
+import Navbar from "./sections/Navbar";
+import Wrapper from "./sections/Wrapper";
+import Footer from "./sections/Footer";
+import Background from "./components/Background";
+import "./scss/index.scss";
 
 function App() {
-  return <div>App</div>;
+  return (
+    <div className="main-container">
+      <Background />
+      <div className="app">
+        <Navbar />
+        <Wrapper />
+        <Footer />
+      </div>
+    </div>
+  );
 }
 
 export default App;
